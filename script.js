878cd6bcace855e761de92e4f0dd3792223bf97b
Yaminisherin <sherinyamini789@gmail.com>
2024-08-21 11:19:27 +0530

    day4 completed

---
+++ script.js
@@ -0,0 +1,62 @@
+/*#dotmethod
+const resume = {
+    name: "Yamini Sherin",
+    age:"21",
+    gender:"Female",
+    martialstatus:"unmarried",
+    phonenumber:9876543210,
+    emailid:"yaminisherin@gmail.com",
+    education: ["bsc.computer science","HSC","SSLC"],
+    skills:["JavaScript","HTML","CSS","BootStrap"],
+  adress : {
+    street:"XXXX",
+    city:"XXXX",
+    state:"XXXX",
+    pincode:987654,
+  }
+    
+}
+console.log(resume);
+console.log(resume.name);
+console.log(resume.age);
+console.log(resume.gender);
+console.log(resume.martialstatus);
+console.log(resume.phonenumber);
+console.log(resume.emailid);
+for(let i=0;i<resume.education.length;i++){
+    console.log(resume.education[i]);
+}
+for(let i=0;i<resume.skills.length;i++){
+    console.log(resume.skills[i]);
+}
+console.log(resume.adress.street);
+console.log(resume.adress.city);
+console.log(resume.adress.state);
+console.log(resume.adress.pincode);
+*/
+/*const resume = {
+    name: "Yamini Sherin",
+    age:"21",
+    gender:"Female",
+    martialstatus:"unmarried",
+    phonenumber:9876543210,
+    emailid:"yaminisherin@gmail.com",
+    education: ["bsc.computer science","HSC","SSLC"],
+    skills:["JavaScript","HTML","CSS","BootStrap"],
+  adress : {
+    street:"XXXX",
+    city:"XXXX",
+    state:"XXXX",
+    pincode:987654,
+  }
+    
+}
+for(let key in resume){
+    console.log(resume[key]);
+}
+resume.education.forEach(ele=>{console.log(ele);})
+resume.skills.forEach(ele=>{console.log(ele);})
+for (let key in resume.adress){
+    console.log(resume.adress[key]);
+}
+*/
